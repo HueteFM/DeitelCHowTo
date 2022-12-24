aea1711ea53390b900eeb53535a579e13a9705e2
HueteFM <huetem@gmail.com>
2022-12-23 22:15:21 -0300

    todos exercicios do cap2 completos

--- cap2/print.c
+++ cap2/print.c
@@ -1,31 +1,40 @@
-/* 2.26 (Separating Digits in an Integer) Write a program that inputs one five-digit
-number, separates the number into its individual digits and displays the digits sepa-
-rated from one another by three spaces each. [Hint: Use combinations of integer di-
-vision and the remainder operation.] For example, if the user types in 42139, the
-program should display
-4 2 1 3 9  */
-
-
+/* 2.29 (Sort in Ascending Order) Write a program that inputs three different numbers
+from the user. Display the numbers in increasing order. Recall that an if statement’s
+body can contain more than one statement. Prove that your script works by running
+it on all six possible orderings of the numbers. Does your script work with duplicate
+numbers? [This is challenging. In later chapters you’ll do this more conveniently and
+with many more numbers.]
+ */
 
 int main(int argc, char const *argv[])
 {
-    int num = 42139;
+    int a, b, c, aux;
+    puts("Input 3 numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
 
-    int dig5 = num / 10000;
+    if(a >= b){
+        aux = a;
+        a = b;
+        b = aux;
+    }
 
-    int prox4 = num % 10000;
-    int dig4 = prox4 / 1000;
+    if(b >= c){
+        aux = c;
+        c = b;
+        b = aux;
+    }
 
-    int prox3 = prox4 % 1000;
-    int dig3 = prox3 / 100;
+    if(a >= b){
+        aux = a;
+        a = b;
+        b = aux;
+    }
 
-    int prox2 = prox3 % 100;
-    int dig2 = prox2 / 10;
+    
 
-    int prox1 = prox2 % 10;
-    int dig1 = prox1 / 1;
+    printf("Os valores em ordem são %d %d %d", a, b, c);
 
-    printf( "%d   %d   %d   %d   %d\n", dig5, dig4, dig3, dig2, dig1);
+
 
     return 0;
 }
